e4e9a2d844f61f4d34d16f5bffd6802b693e70fc
Sabrina Naumovski <snaumovski1@gmail.com>
2025-05-30 14:07:20 -0400

    all the stuff

---
+++ sabrina-portfolio/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		"sabrina-portfolio/index.html",
+		"sabrina-portfolio/src/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
